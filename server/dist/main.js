384f35541b0f9b4007a2dbfab33cd7a8c9041496
infas03 <infasmax03@gmail.com>
2025-02-24 22:54:00 +0530

    updated cors

--- server/dist/main.js
+++ server/dist/main.js
@@ -7,7 +7,7 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'https://cloit-infas-client.vercel.app'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
